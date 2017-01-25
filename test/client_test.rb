4fe1130a80d7e93947c6c9684dca9d345c321488
Christian Kyony <ckyony@changamuka.com>
2017-01-25 09:24:01 +0200

    Test Crossover::Client.post

--- test/client_test.rb
+++ test/client_test.rb
@@ -29,10 +29,12 @@ class CrossoverClientTest < Minitest::Test
   end
 
   def test_post
+    remote_host = TCPServer.new(50000)
     [ 'Hello' ].each do |data,|
       response = Crossover::Client.post(data, '127.0.0.1', '50000')
       response.must_match(/Sending\s+\d+\s+bytes|Sorry!/)
     end
+    remote_host.close
   end
 
 
